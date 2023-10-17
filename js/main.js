cc2d0669a2aee2df8d22027213ffea60e59e9979
Maavel <115097271+Maavel1@users.noreply.github.com>
2023-10-17 17:49:11 +0600

    1

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$('.multiple-items').slick({
+  infinite: true,
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  prevArrow:' <span class="priv_arrow_right"><img src="img/reviews/ARROW RIGHT.png" alt=""></span>',
+  nextArrow:'<span class="priv_arrow"><img src="img/reviews/ARROW LEFT.png" alt=""></span>',
+});
+
